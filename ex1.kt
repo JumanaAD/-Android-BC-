fbef2647ac968fafff571cc773fcde32b31f991a
Jumana Alduhim <107704255+JumanaAD@users.noreply.github.com>
2023-01-01 18:57:05 +0300

    Add files via upload

---
+++ ex1.kt
@@ -0,0 +1,16 @@
+package com.example.jumanaalduhim
+fun main(){
+    var name :String = "abdullah"
+    var fvalue : Float = 20.6F
+    var Dvalue : Double = 8.9746325
+    var b : Byte= 25
+    var Invalue=2022
+    var Lvalue = 321984526954
+    var x: Boolean =true
+    var myChar = 'a'
+
+
+
+
+}
+

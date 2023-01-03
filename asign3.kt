89677771b9311663c74da8500d2ad12df841af9a
Jumana Alduhim <107704255+JumanaAD@users.noreply.github.com>
2023-01-03 23:32:48 +0300

    Add files via upload

---
+++ asign3.kt
@@ -0,0 +1,19 @@
+package com.example.jumanaalduhim
+
+fun main() {
+    var doublearray= arrayListOf<Double>()
+    doublearray.add(3.0)
+    doublearray.add(3.0)
+    doublearray.add(3.0)
+    doublearray.add(3.0)
+    doublearray.add(3.0)
+    var avg =0.0
+    var cal =0.0
+    for(elements in doublearray){
+        cal+=elements
+        avg = (cal) / doublearray.size
+
+
+    }
+    println("avg is $avg")
+}

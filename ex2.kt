149e5c2c5e9c32108e9c1647b5e0c8f40f7048c3
Jumana Alduhim <107704255+JumanaAD@users.noreply.github.com>
2023-01-03 14:40:50 +0300

    Add files via upload

---
+++ ex2.kt
@@ -0,0 +1,24 @@
+package com.example.jumanaalduhim
+
+fun main() {
+
+
+    for (i in 0..100) {
+        if (i == 71)  /// print odd num
+            println("its over 70!!!!")
+        break
+    }
+
+    var temp = 35
+    while (temp >= 10) {
+        when (temp) {
+            30 -> println("$temp too hoot")
+            20 -> println("$temp comfy")
+            15 -> println("$temp cold")
+            else -> {
+                println("the temp is $temp")
+            }
+        }
+        temp--
+    }
+}
